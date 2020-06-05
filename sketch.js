2ea7509c16e886a4669a7943cdc56f40a61ec16e
mhaligowski <mhaligowski@gmail.com>
2020-06-04 23:40:22 -0700

    Code for showing the FFT analysis

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
+let mic, fft;
+
+let ready = false;
+
 function setup() {
-    createCanvas(400, 400);
+  let canvas = createCanvas(710, 400);
+  canvas.mousePressed(mousePressed);
+  noFill();
+
+  mic = new p5.AudioIn();
+  mic.start();
+  fft = new p5.FFT();
+  fft.setInput(mic);
 }
 
 function draw() {
-    background(220);
+  background(200);
+  if (!ready) {
+    return;
+  }
+
+  let spectrum = fft.analyze();
+
+  beginShape();
+  for (i = 0; i < spectrum.length; i++) {
+    vertex(i, map(spectrum[i], 0, 255, height, 0));
+  }
+  endShape();
+}
+
+function mousePressed() {
+  getAudioContext().resume();
 }

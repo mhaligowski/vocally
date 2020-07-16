a1a1ef97251e4f234ef082942efec1d640caad2f
mhaligowski <mhaligowski@gmail.com>
2020-07-16 00:15:21 -0700

    install bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
         include: [SRC_DIR],
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 

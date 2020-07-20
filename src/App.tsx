430694bc401c1c04159479d88be00601f9ab2da4
mhaligowski <mhaligowski@gmail.com>
2020-07-20 11:10:06 -0700

    Add styling for the title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 import { PitchDetection } from "./components/PitchDetection";
 import { Container, Row, Col } from "react-bootstrap";
 
+import clsx from "clsx";
+
 export const App = () => (
   <Container>
     <Row as="header">
-      <h1 className="display-1 text-center mt-5">vocally</h1>
+      <h1 className={clsx("display-1", "text-center", "mt-5", "title")}>vocally</h1>
       <h2 className="text-center">sing C4!</h2>
     </Row>
 

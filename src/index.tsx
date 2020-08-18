b535dd586ae0ba2e9d8027bb20f5e5f377c1da7b
mhaligowski <mhaligowski@gmail.com>
2020-08-18 10:41:32 -0700

    setup Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
 
 import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
@@ -10,5 +11,7 @@ LogRocket.init('6ayqi7/vocallytest', {
   release: 'RELEASE',
 });
 setupLogRocketReact(LogRocket);
+
+Sentry.init({ dsn: 'https://ffe439ac65b84889a37518037a23a7b1@o433557.ingest.sentry.io/5388917' });
 
 ReactDOM.render(<App />, document.getElementById('app'));

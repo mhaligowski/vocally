106b3a0539dfdd9714cf25ab9a5f028216e750ff
mhaligowski <mhaligowski@gmail.com>
2020-08-20 23:29:51 -0700

    configure sentry for testing env

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,31 +1,33 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 // Webpack plugins
-const DynamicCdnWebpackPlugin = require('dynamic-cdn-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const RobotstxtPlugin = require('robotstxt-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const DynamicCdnWebpackPlugin = require("dynamic-cdn-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const RobotstxtPlugin = require("robotstxt-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackDeployPlugin = require("html-webpack-deploy-plugin");
 
-const ROOT_DIR = path.join(__dirname, '..');
-const SRC_DIR = path.join(ROOT_DIR, 'src');
-const ASSETS_DIR = path.join(ROOT_DIR, 'assets');
+const ROOT_DIR = path.join(__dirname, "..");
+const SRC_DIR = path.join(ROOT_DIR, "src");
+const ASSETS_DIR = path.join(ROOT_DIR, "assets");
 
-console.debug('Root directory:\t\t', ROOT_DIR);
-console.debug('Source directory: \t', SRC_DIR);
-console.debug('Assets directory: \t', ASSETS_DIR);
+console.debug("Root directory:\t\t", ROOT_DIR);
+console.debug("Source directory: \t", SRC_DIR);
+console.debug("Assets directory: \t", ASSETS_DIR);
 
 module.exports = {
-  entry: path.resolve(SRC_DIR, 'index.tsx'),
-  target: 'web',
+  entry: path.resolve(SRC_DIR, "index.tsx"),
+  target: "web",
   node: {
-    fs: 'empty',
+    fs: "empty",
   },
 
   externals: [
     {
-      ml5: 'ml5',
-      logrocket: 'LogRocket',
+      ml5: "ml5",
+      logrocket: "LogRocket",
+      "@sentry/browser": "Sentry"
     },
   ],
 
@@ -37,41 +39,55 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-react', '@babel/preset-env'],
-              plugins: ['@babel/plugin-transform-react-display-name'],
+              presets: ["@babel/preset-react", "@babel/preset-env"],
+              plugins: ["@babel/plugin-transform-react-display-name"],
             },
           },
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
           },
         ],
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
 
   plugins: [
     new webpack.ProgressPlugin(),
+    new HtmlWebpackDeployPlugin({
+      packages: {
+        "@sentry/browser": {
+          scripts: {
+            variableName: "Sentry",
+            path: "bundle.min.js",
+          },
+          useCdn: true,
+          getCdnPath: (packageName, packageVersion, packagePath) =>
+            `https://browser.sentry-cdn.com/${packageVersion}/${packagePath}`,
+        },
+      },
+      usePackagesPath: false,
+    }),
     new MiniCssExtractPlugin(),
     new DynamicCdnWebpackPlugin(),
-    new FaviconsWebpackPlugin(path.resolve(ASSETS_DIR, 'favicon.png')),
+    new FaviconsWebpackPlugin(path.resolve(ASSETS_DIR, "favicon.png")),
     new RobotstxtPlugin({
       policy: [
         {
-          userAgent: '*',
-          disallow: '*',
+          userAgent: "*",
+          disallow: "*",
         },
       ],
     }),
   ],
 
   resolve: {
-    modules: [SRC_DIR, 'node_modules'],
-    extensions: ['.ts', '.tsx', '.js'],
+    modules: [SRC_DIR, "node_modules"],
+    extensions: [".ts", ".tsx", ".js"],
   },
 };
--- config/webpack.testing.js
+++ config/webpack.testing.js
@@ -1,30 +1,30 @@
-const { DefinePlugin } = require('webpack');
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { DefinePlugin } = require("webpack");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const path = require('path');
-const common = require('./webpack.config.js');
+const path = require("path");
+const common = require("./webpack.config.js");
 
-const ROOT_DIR = path.join(__dirname, '..');
-const SRC_DIR = path.join(ROOT_DIR, 'src');
+const ROOT_DIR = path.join(__dirname, "..");
+const SRC_DIR = path.join(ROOT_DIR, "src");
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'inline-source-map',
+  mode: "production",
+  devtool: "inline-source-map",
 
   resolve: {
     alias: {
-      log: path.resolve(SRC_DIR, 'log.testing.ts'),
+      log: path.resolve(SRC_DIR, "log.testing.ts"),
     },
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(SRC_DIR, 'index.html'),
-      title: 'sing vocally',
+      template: path.resolve(SRC_DIR, "index.html"),
+      title: "sing vocally",
     }),
     new DefinePlugin({
-      RELEASE: '0.1',
+      RELEASE: "0.1",
     }),
   ],
 });
